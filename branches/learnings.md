f7d4452eea6ecfb0526e92a5786aa89d63bcb506
sangnovamoqapi <morjariyajigar45@gmail.com>
2024-04-18 01:00:54 +0530

    branches

---
+++ branches/learnings.md
@@ -0,0 +1,3 @@
+1. loop {  } is useful in cases like knowing if a thread has completed its job.
+2. We might need value from the loop outside which can be given after break.
+3. 
---
+++ branches/src/main.rs
@@ -0,0 +1,48 @@
+fn main() {
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
+    
+    // Loops with break
+    {
+        let mut counter = 0;
+
+        let result = loop {
+            counter += 1;
+            if counter == 10 {
+                break counter * 2;
+            }
+        };
+        println!("The result is {result}");
+    }
+    
+    // Named loops
+    {
+        let mut count = 0;
+        'counting_up: loop {
+            println!("count = {count}");
+            let mut remaining = 10;
+
+            loop {
+                println!("remaining = {remaining}");
+                if remaining == 9 {
+                    break;
+                }
+                if count == 2 {
+                    break 'counting_up;
+                }
+                remaining -= 1;
+            }
+            count += 1;
+        }
+        println!("End count = {count}");
+    }
+    
+    // for loop
+    {
+        for number in (1..4).rev() {
+            println!("{number}!");
+        }
+    }
+}
